a2a27d66071c9a7a408e9610f9389cb41d39eab2
Seth Bertlshofer <celestials2013@gmail.com>
2017-03-30 11:25:31 -0600

    changed function to arrow functions, added 'use strict;' where necessary

--- express_server/routes/index.js
+++ express_server/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next)=>{
+    'use strict';
+
     res.render('index', { title: 'Home Screen', hide_return_to_main_menu: true });
 });
 

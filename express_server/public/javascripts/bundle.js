a997e9ab1ba1d52526216ba1dccac9fecefe2f1c
Seth Bertlshofer <celestials2013@gmail.com>
2017-04-01 09:39:08 -0600

    added missing 'use strict'

--- express_server/public/javascripts/bundle.js
+++ express_server/public/javascripts/bundle.js
@@ -9,18 +9,28 @@ var about = require("./views/about.hbs");
 var eApp = document.getElementById("app");
 
 page('/', ()=>{
+    'use strict';
+
     eApp.innerHTML = index({});
 });
 page('/game', ()=>{
+    'use strict';
+
     eApp.innerHTML = game({});
 });
 page('/about', ()=>{
+    'use strict';
+
     eApp.innerHTML = about({title : "about"});
 });
 page('/help', ()=>{
+    'use strict';
+
     eApp.innerHTML = help({title : "help"});
 });
 page('/highscores', ()=>{
+    'use strict';
+
     eApp.innerHTML = highscores({title : "highscores", highscores : [45, 23, 12, 9, 6, 3]});
 });
 

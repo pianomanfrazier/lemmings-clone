a72e92d4dcfa3a166f056d35aac38ef6e3ae14fa
Seth Bertlshofer <celestials2013@gmail.com>
2017-04-08 12:14:24 -0600

    removed bundle.js

--- server/public/javascripts/bundle.js
+++
@@ -1,3 +0,0 @@
-{"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<ul id=\"main-menu\">\n    <li class=\"btn bump\">\n        <a href=\"/game\">New Game</a>\n    </li>\n    <li class=\"btn bump\">\n        <a href=\"/about\">About</a>\n    </li>\n    <li class=\"btn bump\">\n        <a href=\"/help\">Help</a>\n    </li>\n    <li class=\"btn bump\">\n        <a href=\"/highscores\">High Scores</a>\n    </li>\n</ul>\n";
-},"useData":true}

82ed272d8fb4096e1c15dd61c977b4e3744842ed
Ryan Frazier <pianomanfrazier@gmail.com>
2017-04-22 18:03:51 -0600

    create level object from csv

---
+++ client/js/levels/level1.js
@@ -0,0 +1,29 @@
+let Papa = require("papaparse");
+let _    = require("lodash");
+let level1 =
+{
+    "lemmingGoal": 10,
+    "lemmingTypes": {
+        "climbing": 10,
+        "umbrella": 10
+    },
+    "map": []
+};
+
+Papa.parse("/levels/level1.csv", {
+    delimiter: ",",
+    newline: "\n",
+    download: true,
+    error: (e)=>{
+        'use strict';
+        console.log(e);
+    },
+    complete: (results)=>{
+        'use strict';
+        console.log("parse csv complete for level1");
+        console.log(results);
+        level1.map = results.data;
+    }
+});
+
+module.exports = level1;

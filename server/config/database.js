bb5b9054a0bd504f9bf98c0179744608a428ca93
Ryan Frazier <pianomanfrazier@gmail.com>
2018-02-15 13:44:36 -0700

    add license and docker stuffs

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
     // start a mongo instance with `docker run --name lemmings-mongo -p 27017:27017 -d mongo`
-    'url' : 'mongodb://localhost/lemmings'
+    //'url' : 'mongodb://localhost/lemmings'
+    // or connect with docker-compose
+    'url' : 'mongodb://mongo/lemmings'
     // or set up authentication
     //'url' : 'mongodb://lemming:SuperSecret12345@localhost:27017/lemmings'
 };
